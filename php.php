48c0fd0b8a97c8abb15d3529c46a0b073a578a9b
Dominik Buczkowski <97897239+DominikBuczkowski@users.noreply.github.com>
2022-06-15 17:28:48 +0200

    Update php.php

--- php.php
+++ php.php
@@ -3,6 +3,7 @@ ini_set('display_errors',0);
 
 if( isset( $_GET['wyslij'] ))
 {
+  include "index.html";
   $operator=$_REQUEST['operator'];
   $n1 = $_GET['liczba1'];
   $n2 = $_GET['liczba2'];
@@ -20,6 +21,10 @@ if( isset( $_GET['wyslij'] ))
   }
 
   if($operator=="/") {
+    if($n1 == 0 || $n2 == 0) {
+      echo "<script language=javascript> alert(\"NIE MOŻNA DZIELIĆ PRZEZ 0\");</script>";
+      
+    }
   $res= $n1/$n2;
   }
 
@@ -28,6 +33,9 @@ if( isset( $_GET['wyslij'] ))
       echo "<script language=javascript> alert(\"NIE WPROWADZONO LICZBY\");</script>";
     }
 
-  echo $res;
+  echo '<p id="pp">'.$res.'</p>';
 }
+
+/* com multilines*/
+// or # com singleline
 ?>
